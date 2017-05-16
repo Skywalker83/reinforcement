28b5be14a93ab7bbb2c1e4bac94e05a054b4e9e1
Skywalker83 <ccc_1983@hotmail.com>
2017-05-16 18:25:50 +0800

    Created FrozenLake

--- gym/FrozenLake.py
+++ gym/FrozenLake.py
@@ -1,17 +1,18 @@
 import gym
 
-env = gym.make('FrozenLake-v0')
 
+env = gym.make('FrozenLake-v0')
+print("Action space: {}".format(env.action_space))
+print("Observation space: {}".format(env.observation_space))
 
 for i_episode in range(20):
     observation = env.reset()
     for t in range(100):
         env.render()
-        print(observation)
-
         action = env.action_space.sample()
         observation, rewards, done,info = env.step(action)
+
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
+            print("Episode {} finished after {} timesteps".format(i_episode+1,t+1))
             break
 
