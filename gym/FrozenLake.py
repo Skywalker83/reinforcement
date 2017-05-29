b89382a67c2a7d868608665aa295b3c1d2edb3a5
skywalker83 <jhmj3347>
2017-05-29 18:16:20 +0800

    Amend configuration parameters

--- gym/FrozenLake.py
+++ gym/FrozenLake.py
@@ -22,8 +22,20 @@ class Config:
         self.decay_exploration_steps = 10**4
         self.replay_buffer = 5 * 10**5
         self.gamma = 0.99
-        self.gpu = 0;
+        self.gpu = 0
         self.start_epsilon = 0.99
+        self.replay_start_size = 50
+        self.mini_batch_size = 4
+        self.update_frequency = 1
+        self.target_update_frequency = 100
+        self.episodic_update_len = 16
+        self.episodic_update = False
+        self.average_loss_decay = 0.99
+        self.average_q_decay = 0.999
+        self.soft_update_tau = 0.01
+        self.clip_delta = True
+
+
 
 
 #Setting up of Gym Environment
@@ -67,20 +79,20 @@ def setupAgent(env,config):
         config.gamma,
         explorer,
         gpu=config.gpu,
-        replay_start_size=50000,
-        minibatch_size=32,
-        update_frequency=1,
-        target_update_frequency=10000,
-        clip_delta=True,
+        replay_start_size=config.replay_start_size,
+        minibatch_size=config.mini_batch_size,
+        update_frequency=config.update_frequency,
+        target_update_frequency=config.target_update_frequency,
+        clip_delta=config.clip_delta,
         phi=phi,
         target_update_method=u'hard',
-        soft_update_tau=0.01,
+        soft_update_tau=config.soft_update_tau,
         n_times_update=1,
-        average_q_decay=0.999,
-        average_loss_decay=0.99,
+        average_q_decay=config.average_q_decay,
+        average_loss_decay=config.average_loss_decay,
         batch_accumulator=u'mean',
-        episodic_update=4,
-        episodic_update_len=16)
+        episodic_update=config.episodic_update,
+        episodic_update_len=config.episodic_update_len)
 
 
 def main():
